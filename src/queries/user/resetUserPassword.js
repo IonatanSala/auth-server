fb2478e91208511f3cb5f129f29a70c74b5b5e54
Ionatan Sala <sala.jonatan1995@gmail.com>
2017-04-19 21:39:59 +0100

    fis issue with null user

--- src/queries/user/resetUserPassword.js
+++ src/queries/user/resetUserPassword.js
@@ -2,7 +2,15 @@ const User = require('../../models/User');
 
 const resetUserPassword = (_id, resetPasswordKey, password) => {
   password = User.hashPassword(password);
-  return User.findOneAndUpdate({ _id, resetPasswordKey}, { password, resetPasswordKey: null });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOneAndUpdate({ _id, resetPasswordKey}, { password, resetPasswordKey: null });
+      if(!user) return reject();
+      resolve();
+    } catch(e) {
+      reject();
+    }
+  });
 }
 
 module.exports = resetUserPassword;
