c8310828f4da8521bbcc0af34ee3929261ec1719
Ionatan Sala <sala.jonatan1995@gmail.com>
2017-04-20 20:59:47 +0100

    enabel cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const morgan = require('morgan');
+const cors = require('cors');
+const { CLIENT_URL } = require('./config');
 const router = require('./routes/router');
 const app = express();
 // authentication services setup
@@ -9,8 +11,16 @@ require('./services/authentication');
 // setup database
 const DBSetup = require('./config/database');
 DBSetup()
+  // database is connected
   .then(() => {
+
+    const corsOptions = {
+      origin: CLIENT_URL,
+      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    };
+
     app.use(morgan('combined'));
+    app.use(cors(corsOptions));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json({ type: "application/json" }));
     app.use(passport.initialize());
@@ -19,6 +29,7 @@ DBSetup()
     });
     router(app);
   })
+  // couldn't connect to the database
   .catch((e) => {
     app.use((req, res, next) => {
       res.send('Something went wrong please try again later.');
